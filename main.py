0c1d9ed02f315201f2e1ecf37ec7daecaa318fc1
vxb762 <129900052+vxb762@users.noreply.github.com>
2023-04-04 13:13:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ servo = Servo(17)
 
 def main():
     while True:
-        servo.value = 0.5
+        servo.value = 1.0
         sleep(0.5)
-        servo.value = -0.5
+        servo.value = -1.0
         sleep(0.5)
 
 if __name__ == "__main__":
